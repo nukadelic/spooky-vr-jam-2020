955bf5ca9ea448bad4ee64b0eb663bc7844e7cd8
Chris-crass <christopher_briand@hotmail.com>
2020-10-24 02:55:41 -0300

    candy kenesis start

--- Assets/Project/Scripts/CandyKenesis.cs
+++ Assets/Project/Scripts/CandyKenesis.cs
@@ -4,15 +4,54 @@ using UnityEngine;
 
 public class CandyKenesis : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public List<Rigidbody> candyHoard = new List<Rigidbody>();
+    public Rigidbody candyAnchor;
+
+    public void RegisterCandy(Rigidbody candyRB)
+    {
+        candyHoard.Add(candyRB);
+        CreateJoint(candyRB.GetComponent<Rigidbody>());
+    }
+    public void UnregisterCandy(Transform candyTrans)
+    {
+        candyHoard.Remove(candyTrans);
+
+    }
     void Start()
     {
-        
+        jd.positionDamper = 1f;
+        limit.limit = 0.38f;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
+        foreach()
+    }
+
+    private JointDrive jd = new JointDrive();
+    private SoftJointLimit limit = new SoftJointLimit();
+    public void CreateJoint(Rigidbody rb)
+    {
+        var tempJoint = rb.gameObject.AddComponent<ConfigurableJoint>();
+        rb.transform.position = candyAnchor.position;
+        tempJoint.anchor = Vector3.zero;
+        tempJoint.connectedBody = candyAnchor;
+
+        tempJoint.xMotion = ConfigurableJointMotion.Limited;
+        tempJoint.yMotion = ConfigurableJointMotion.Limited;
+        tempJoint.zMotion = ConfigurableJointMotion.Limited;
+
+        tempJoint.linearLimit = limit;
+
+        tempJoint.angularXDrive = jd;
+        tempJoint.angularYZDrive = jd;
+        tempJoint.xDrive = jd;
+        tempJoint.yDrive = jd;
+        tempJoint.zDrive = jd;
+
+        tempJoint.enableCollision = false;
+        tempJoint.enablePreprocessing = false;
         
     }
 }
