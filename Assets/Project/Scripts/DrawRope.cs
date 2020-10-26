62a6873f706991bb45db76aaea8c29830a3a9a9b
Nukadelic <vlad.voina@gmail.com>
2020-10-26 15:08:25 +0700

    draw rope

--- Assets/Project/Scripts/DrawRope.cs
+++ Assets/Project/Scripts/DrawRope.cs
@@ -5,10 +5,12 @@ using SplineMesh;
 
 public class DrawRope : MonoBehaviour
 {
-    public Spline spline;
-    public SplineMeshTiling meshTiling;
+    Spline spline;
+    SplineMeshTiling meshTiling;
 
-    // Start is called before the first frame update
+    public Transform targetA;
+    public Transform targetB;
+    
     void Start()
     {
         spline = GetComponentInChildren<Spline>();
@@ -16,15 +18,52 @@ public class DrawRope : MonoBehaviour
 
         var c = spline.nodes.Count;
 
-        //while( spline.nodes.Count > 0 )
-        {
-            //spline.AddNode( new SplineNode() )
-        }
+        //meshTiling.updateInPlayMode = true;
+
+        while( spline.nodes.Count > 2 ) spline.RemoveNode( spline.nodes[ 0 ] );
+        while( spline.nodes.Count < 2 ) spline.AddNode( new SplineNode( Vector3.zero, Vector3.up ) );
     }
 
-    // Update is called once per frame
+    public void StopDrawing()
+    {
+        gameObject.SetActive( false );
+    }
+
+    public void SetPoints( Vector3 pointA, Vector3 pointB )
+    {
+        if( ! gameObject.activeSelf )
+            gameObject.SetActive( true );
+
+        var nodeA = spline.nodes[ 0 ];
+        var nodeB = spline.nodes[ 1 ];
+        
+        var pA = pointA - this.transform.position;
+        var pB = pointB - this.transform.position;
+
+        var delta = pB - pA;
+
+        nodeA.Position = pointA - this.transform.position;
+        nodeB.Position = pointB - this.transform.position;
+        
+        if( pA.y > pB.y )
+        {
+            nodeA.Direction = pA + Vector3.down * delta.magnitude / 4f;
+            nodeB.Direction = pB + delta / 2f;
+        }
+        else
+        {
+            nodeB.Direction = pB + Vector3.down * delta.magnitude / 4f;
+            nodeA.Direction = pA + delta / 2f;
+        }
+        
+        //spline.RefreshCurves();
+    }
+    
     void Update()
     {
-        
+        if( targetA != null && targetB != null )
+        {
+            SetPoints( targetA.transform.position, targetB.transform.position );
+        }
     }
 }
