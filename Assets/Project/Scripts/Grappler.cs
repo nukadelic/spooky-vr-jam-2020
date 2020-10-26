7f7b836cb2d280a5a105699ed2f8fd51048b15e0
Chris-crass <christopher_briand@hotmail.com>
2020-10-25 23:36:40 -0300

    ongoing pumpkin and level

---
+++ Assets/Project/Scripts/Grappler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grappler : MonoBehaviour
+{
+    public Rigidbody grappleBody;
+    public ConfigurableJoint grappleJoint;
+    public Transform aimIndicator;
+    public Transform aimerTransform;
+
+    void FixedUpdate()
+    {
+        // Bit shift the index of the layer (8) to get a bit mask
+        int layerMask = 1 << 8;
+
+        // This would cast rays only against colliders in layer 8.
+        // But instead we want to collide against everything except layer 8. The ~ operator does this, it inverts a bitmask.
+        //layerMask = ~layerMask;
+
+        RaycastHit hit;
+        // Does the ray intersect any objects excluding the player layer
+        if (Physics.Raycast(grappleBody.position, aimerTransform.forward, out hit, 20f, layerMask))
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.yellow);
+            Debug.Log("Did Hit");
+        }
+        else
+        {
+            Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
+            Debug.Log("Did not Hit");
+        }
+    }
+}
